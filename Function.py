7417e7c4b68446adb02e8201502a9a57c2fdf6f7
DemoniaFranjuda <00001106703194sp@al.educacao.sp.gov.br>
2025-04-29 10:17:59 -0300

    Teste commit

---
+++ Function.py
@@ -0,0 +1,36 @@
+def soma():
+
+ num01 = 10
+ num02 = 20
+
+ resultado = num01 + num02
+
+ return resultado
+
+def somarDoisNumeros(nota_01, nota_02):
+ num_01 = nota_01
+ num_02 = nota_02
+
+ resultado = num_01 + num_02
+
+ return print(resultado)
+
+#somarDoisNumeros(24,98)
+
+
+def multiplicacao():
+ 
+ num_01 = 44
+ num_02 = 6
+
+ resultado = num_01 * num_02
+
+ return resultado 
+
+def multiplicarDoisNumeros (nota_01, nota_02):
+ num_01 = nota_01
+ num_02 = nota_02
+ resultado = num_01 + num_02
+ return print(resultado)
+
+multiplicarDoisNumeros(24,98)
